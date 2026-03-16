d897fc3824066d16743f301af0bfe758e52b32ce
chyngyz11-11 <belek4301@gmail.com>
2026-03-16 14:21:01 +0600

    lesson-10

--- app.js
+++ app.js
@@ -1,7 +1,53 @@
-const links = document.querySelectorAll(".item > a")
-
-for(let i = 0; i < links.length; i++){
-    links[i].onclick = function(){
-        document.getElementById(links[i].getAttribute ("data-link")).scrollIntoView({behavior: "smooth"})
-    }
+const name = "Chyngyz"
+let age = 14
+let online = false
+let user = {
+    name: "Chyngyz",
+    age: 14,
+    online: false
 }
+console.log(name, typeof name)
+console.log(age, typeof age)
+console.log(online, typeof online)
+console.log(user, typeof user)
+
+
+
+let phone = {
+    brand: "Samsung",
+    model: "Galaxy S24",
+    price: 50000
+}
+console.log(`Телефон: ${phone.brand} ${phone.model} ${phone.price}`)
+
+
+
+let book = {
+    title: "Мастер и Маргарита",
+    author: "Михаил Булгаков",
+    year: 1967
+}
+console.log(`Книга: ${book.title} ${book.author} ${book.year}`)
+
+
+
+let student = {
+    name: "Acaн",
+    grade: 5,
+    subject: "Математика"
+}
+console.log(` ${student.name} получил ${student.grade} по предмету ${student.subject}`)
+
+
+
+let car = {
+    brand: "Toyota",
+    model: "Camry",
+    year: 2020,
+    owner: {
+        name: "Айбек",
+        age: 30
+    }
+
+}
+console.log(`Машина: ${car.brand} ${car.model} ${car.year}, ${car.owner.age}`)
