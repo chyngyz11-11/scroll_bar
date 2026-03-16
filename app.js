25a574995eb3cc13087baf5cf0f2648eca8addc2
chyngyz11-11 <belek4301@gmail.com>
2026-03-16 14:18:25 +0600

    lesson-6

--- app.js
+++ app.js
@@ -1,28 +1,72 @@
-const form = document.querySelector(".form")
-const inputs = document.querySelectorAll("input")
-const result_summary = document.querySelector(".result_summary")
+let price = 250
+let products = 4
+const result = price * products
+console.log(`The total cost is: ${result}`)
 
-form.addEventListener("submit", function (e) {
-    e.preventDefault()
 
-    const weekly_summary_spenses = []
 
-    inputs.forEach((input) => {
-        if (Number(input.value) === 0) {
-            alert("its empty")
-        }else {
-           weekly_summary_spenses.push(Number(input.value))
-           input.value = ""
+let width = 12
+let height = 6
+const area = width * height
+console.log(`Rectangle area: ${area}`)
 
-        }
-    })
 
-    const total = weekly_summary_spenses.reduce((sum, value) => sum + value, 0)
 
-    const average = total / weekly_summary_spenses.length
+let age = 18
+let after = 5
+const will  = age + after
+console.log(`After 5 years i will be: ${will}`)
 
-    result_summary.innerHTML = `
-        <h2 class="total">Total:${total} som</h2>
-        <strong class="average">Average:${average.toFixed(2)} som</strong>
-    `
-})
+
+
+let cost = 30000
+let money = 45000
+const left =  money - cost
+console.log(`money left: ${left}`)
+
+
+
+let car = 120
+let speed = 2
+const km = car / speed
+console.log(`Average speed: ${km}`)
+
+
+let a = 2;
+let b = 3;
+let sum = a + b;
+console.log("Сумма чисел:", sum);
+
+
+let coste = 100;
+let quantity = 2;
+let totalCost = coste * quantity;
+console.log("Общая стоимость покупки:", totalCost);
+
+
+
+let a1 = 4;
+let a2 = 6;
+let a3 = 8;
+let average = (a1 + a2 + a3) / 3;
+console.log("Среднее арифметическое:", average);
+
+
+
+let length = 5;
+let widt = 4;
+let perimeter = (length + widt) * 2;
+console.log("Периметр прямоугольника:", perimeter);
+
+
+
+let x = 1;
+let y = 2;
+
+let t = x;
+x = y;
+y = t;
+
+console.log("x =", x);
+console.log("y =", y);
+
